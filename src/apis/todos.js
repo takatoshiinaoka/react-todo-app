a8c7568bfffb2b2f8d9d5525e22b033055b6e043
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-14 14:07:01 +0900

    [add] Communicate with Mock Servers

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,2 +1,26 @@
 // "./apis" <= モックサーバーと通信するファイルを格納するディレクトリ
 // "todos.js" <= サーバーとの通信用ファイル
+// 作成したモックサーバーとの通信に axios を利用する
+import axios from "axios"
+
+const todoDataUrl = "http://localhost:3100/todos"
+
+export const getAllTodosData = async () => {
+    const response =  await axios.get(todoDataUrl)
+    return response.data
+}
+
+export const addTodoData = async (todo) => {
+    const response = await axios.post(todoDataUrl, todo)
+    return response.data
+}
+
+export const deleteTodoData = async (id) => {
+    await axios.delete(`${todoDataUrl}/${id}`)
+    return id
+}
+
+export const updateTodoData = async (id, todo) => {
+    const response = await axios.put(`${todoDataUrl}/${id}`, todo)
+    return response.data
+}
