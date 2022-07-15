6968c090174a8fd79603618035ec947a9e45b821
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-15 10:55:14 +0900

    [add] resetCSS={false}

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
     <StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme} resetCSS={false}>
             <App/>
         </ChakraProvider>
     </StrictMode>
